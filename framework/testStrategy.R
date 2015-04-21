3465dec8d3235f83a42f528fd671fe78672ad5a0
Roberto <rmartin.martin@gmail.com>
2015-04-20 23:35:07 -0400

    adding todo

--- framework/testStrategy.R
+++ framework/testStrategy.R
@@ -9,7 +9,7 @@ if(!exists("PerformanceTable", mode="function")) source("strategyMetrics.R") #st
 if(!exists("OrderPerformanceTable", mode="function")) source("strategyMetrics.R") #strategy
 if(!exists("SelectTopNStrategies", mode="function")) source("strategyMetrics.R") #strategy
 if(!exists("FindOptimumStrategy", mode="function")) source("strategyMetrics.R") #strategy
- source("strategyData.R") #strategy
+if(!exists("StrategyData", mode="function")) source("strategyData.R") #strategy
 
 
 nameOfStrategy <- "GSPC Moving Average Strategy"
@@ -20,11 +20,6 @@ trainingEndDate <- as.Date("2010-01-01")
 outofSampleStartDate <- as.Date("2010-01-02")
 
 #1. Get Data
-#symbolData <- new.env() #store data in a separate environment
-#getSymbols("^GSPC", env = symbolData, src = "yahoo", from = trainingStartDate )
-#trainingData <- window(symbolData$GSPC, start = trainingStartDate, end = trainingEndDate )
-#testData <- window(symbolData$GSPC, start = outofSampleStartDate)
-
 trainingData = StrategyData("GSPC", trainingStartDate, trainingEndDate, outofSampleStartDate)$trainingData
 testData <- StrategyData("GSPC", trainingStartDate, trainingEndDate, outofSampleStartDate)$testData
 
@@ -32,10 +27,9 @@ testData <- StrategyData("GSPC", trainingStartDate, trainingEndDate, outofSample
 indexReturns <- Delt(Cl(window(stockData$GSPC, start = outofSampleStartDate))) #calculate returns for out of sample
 colnames(indexReturns) <- "GSPC Buy & Hold"
 
-
 pTab <- FindOptimumStrategy(trainingData) #performance table of each strategy
 
-#test
+#test: TODO select top strategy and test against benchmark
 dev.new()
 #manually specify a strategy
 outofSampleReturns <- TradingStrategy(testData, mavga_period=9, mavgb_period=6)

4b219fef7f424981175210dcc6959a5c1770d317
Roberto <rmartin.martin@gmail.com>
2015-04-22 02:55:32 -0400

    changing dates

--- framework/testStrategy.R
+++ framework/testStrategy.R
@@ -16,22 +16,23 @@ nameOfStrategy <- "AAPL Moving Average Strategy"
 
 #specify dates
 trainingStartDate <- as.Date("2007-01-01")
-trainingEndDate <- as.Date("2013-06-19")
-outofSampleStartDate <- as.Date("2013-06-20")
+trainingEndDate <- as.Date("2012-06-19")
+outofSampleStartDate <- as.Date("2012-06-20")
+outofSampleEndDate <- as.Date("2014-01-01")
 
 #1. Get Data
-trainingData = StrategyData("AAPL", trainingStartDate, trainingEndDate, outofSampleStartDate)$trainingData
-testData <- StrategyData("AAPL", trainingStartDate, trainingEndDate, outofSampleStartDate)$testData
+trainingData = StrategyData("AAPL", trainingStartDate, trainingEndDate, outofSampleStartDate, outofSampleEndDate)$trainingData
+testData <- StrategyData("AAPL", trainingStartDate, trainingEndDate, outofSampleStartDate, outofSampleEndDate)$testData
 
 #index returns
-indexReturns <- Delt(Cl(window(stockData$GSPC, start = outofSampleStartDate))) #calculate returns for out of sample
-colnames(indexReturns) <- "GSPC Buy & Hold"
+indexReturns <- Delt(Cl(window(stockData$AAPL, start = outofSampleStartDate, end = outofSampleEndDate))) #calculate returns for out of sample
+colnames(indexReturns) <- "AAPL Buy & Hold"
 
 pTab <- FindOptimumStrategy(trainingData) #performance table of each strategy
 
 #test: TODO select top strategy and test against benchmark
 dev.new() #doesn't work in rstudio
 #manually specify a strategy
-outofSampleReturns <- TradingStrategy(testData, mavga_period=2, mavgb_period=5)
+outofSampleReturns <- TradingStrategy(testData, mavga_period=11, mavgb_period=24)
 finalReturns <- cbind(outofSampleReturns, indexReturns)
 charts.PerformanceSummary(finalReturns, main=paste(nameOfStrategy, "- Out of Sample"), geometric=FALSE)
